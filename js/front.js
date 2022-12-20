4cf8f0057282057c46fd5a76395b2004d0824430
Srishti Pandey <srishti.pandey@gmail.com>
2022-12-20 09:09:13 +0530

    first commit

---
+++ js/front.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+  Drupal.behaviors.initSlider = {
+    attach: function (context, settings) {
+
+      $(document).ready(function(){
+        $('.slick-slider').slick({
+          dots: true,
+          speed: 800,
+          autoplay: true,
+        });
+      });
+
+    }
+  };
+
+})(jQuery);
